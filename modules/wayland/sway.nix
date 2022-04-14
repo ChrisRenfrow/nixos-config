501ba3af6c2e3a989dbcc6e8d4a0afdc7ab2c2ea
Chris Renfrow <dev@chrisrenfrow.me>
2022-04-13 17:50:32 -0700

    Enable and configure wlr screencast setting

--- modules/wayland/sway.nix
+++ modules/wayland/sway.nix
@@ -52,11 +52,11 @@ in {
         wlr = {
           enable = true;
           settings = {
-            # screencast = {
-            #   max_fps = 30;
-            #   chooser_type = "simple";
-            #   chooser_cmd = "${pkgs.slurp}/bin/slurp -f %o -or";
-            # };
+            screencast = {
+              max_fps = 30;
+              chooser_type = "simple";
+              chooser_cmd = "${pkgs.slurp}/bin/slurp -f %o -or";
+            };
           };
         };
 
