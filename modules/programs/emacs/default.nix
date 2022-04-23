9c8e1b7b127a049061737ecc7907e26a1375d9f6
Chris Renfrow <dev@chrisrenfrow.me>
2022-04-22 20:05:14 -0700

    Add new emacs definition plus current emacs config files
    Uses emacs-overlay's `emacsWithPackagesFromUsePackage` function to
    read init.el and automagically compile my emacs with the necessary
    packages from the overlay.

---
+++ modules/programs/emacs/default.nix
@@ -0,0 +1,32 @@
+{ config, options, pkgs, pkgsLocal, hmLib, lib, inputs, ... }:
+
+let
+  inherit (lib) mkIf mkEnableOption;
+  inherit (builtins) readFile;
+
+  cfg = config.modules.programs.emacs;
+in {
+
+  options.modules.programs.emacs.enable = mkEnableOption "emacs";
+
+  config = mkIf cfg.enable {
+
+    nixpkgs.overlays = with inputs; [ (import emacs-overlay) ];
+
+    system.user.hm.home.file.".emacs.d/early-init.el".text = readFile ./early-init.el;
+    system.user.hm.home.file.".emacs.d/init.el".text = readFile ./init.el;
+
+    system.user.hm = {
+      programs.emacs = {
+        enable = true;
+        package = pkgs.emacsWithPackagesFromUsePackage {
+          alwaysEnsure = true;
+          package = pkgs.emacsPgtkGcc;
+          config = ./init.el;
+        };
+      };
+    };
+
+    environment = { systemPackages = with pkgs; [ ]; };
+  };
+}
